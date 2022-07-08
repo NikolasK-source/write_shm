7efeb5ae46068933463be86ab7cc15b2bd712c4a
Nikolas Koesling <nikolas@koesling.info>
2022-07-08 13:17:11 +0200

    add shm class from https://github.com/NikolasK-source/stdin_to_modbus_shm

---
+++ src/SHM.cpp
@@ -0,0 +1,45 @@
+#include "SHM.hpp"
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <system_error>
+#include <unistd.h>
+
+#include <utility>
+
+
+SHM::SHM(std::string name) : name(std::move(name)) {
+    // open shared memory object
+    fd = shm_open(this->name.c_str(), O_RDWR, 0660);
+    if (fd < 0) {
+        throw std::system_error(errno, std::generic_category(), "Failed to open shared memory '" + this->name + '\'');
+    }
+
+    // get size of shared memory object
+    struct stat shm_stats {};
+    if (fstat(fd, &shm_stats)) {
+        if (close(fd)) { perror("close"); }
+        fd = -1;
+        throw std::system_error(
+                errno, std::generic_category(), "Failed to read stats of shared memory '" + this->name + '\'');
+    }
+    size = static_cast<size_t>(shm_stats.st_size);
+
+    // map shared memory
+    addr = mmap(nullptr, size, PROT_WRITE, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED || addr == nullptr) {
+        if (close(fd)) { perror("close"); }
+        addr = nullptr;
+        throw std::system_error(errno, std::generic_category(), "Failed to mmap shared memory '" + this->name + '\'');
+    }
+}
+
+SHM::~SHM() {
+    if (addr) {
+        if (munmap(addr, size)) { perror("munmap"); }
+    }
+    if (fd != -1) {
+        if (close(fd)) { perror("close"); }
+    }
+}
---
+++ src/SHM.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+
+#include <string>
+class SHM {
+private:
+    std::string name;
+    int         fd   = -1;
+    std::size_t size = 0;
+    void *      addr = nullptr;
+
+public:
+    explicit SHM(std::string name);
+
+    ~SHM();
+
+    /*! \brief get size of the shared memory object
+     *
+     * @return size in bytes
+     */
+    [[nodiscard]] size_t get_size() const { return size; }
+
+    /*! \brief get address to the shared memory
+     *
+     * caller is responsible for selecting the right element type!
+     *
+     * @tparam type element type
+     * @return address
+     */
+    template <typename type = void *>
+    [[nodiscard]] type get_addr() {
+        static_assert(std::is_pointer<type>::value, "Expected a pointer type");
+        return reinterpret_cast<type>(addr);
+    }
+
+    /*! \brief get name of the shared memory object
+     *
+     * @return name
+     */
+    [[nodiscard]] const std::string &get_name() const { return name; }
+};
