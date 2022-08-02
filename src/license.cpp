5fcbbdf83d46ccf86c0308d0ba894228b01c0f27
Nikolas Koesling <nikolas@koesling.info>
2022-08-02 12:47:15 +0200

    add copyright file header and licenses

---
+++ src/license.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2022 Nikolas Koesling <nikolas@koesling.info>.
+ * This program is free software. You can redistribute it and/or modify it under the terms of the MIT License.
+ */
+
+#include "license.hpp"
+
+void print_licenses(std::ostream &o) {
+    o << "This Application (" << PROJECT_NAME << ' ' << PROJECT_VERSION "): " << std::endl;
+    o << std::endl;
+    o << "    MIT License" << std::endl;
+    o << std::endl;
+    o << "    Copyright (c) 2021-2022 Nikolas Koesling" << std::endl;
+    o << std::endl;
+    o << "    Permission is hereby granted, free of charge, to any person obtaining a copy" << std::endl;
+    o << "    of this software and associated documentation files (the \"Software\"), to deal" << std::endl;
+    o << "    in the Software without restriction, including without limitation the rights" << std::endl;
+    o << "    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" << std::endl;
+    o << "    copies of the Software, and to permit persons to whom the Software is" << std::endl;
+    o << "    furnished to do so, subject to the following conditions:" << std::endl;
+    o << std::endl;
+    o << "    The above copyright notice and this permission notice shall be included in all" << std::endl;
+    o << "    copies or substantial portions of the Software." << std::endl;
+    o << std::endl;
+    o << "    THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" << std::endl;
+    o << "    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," << std::endl;
+    o << "    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" << std::endl;
+    o << "    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" << std::endl;
+    o << "    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," << std::endl;
+    o << "    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE" << std::endl;
+    o << "    SOFTWARE." << std::endl;
+    o << std::endl;
+    o << std::endl;
+    o << "cxxopts Library (https://github.com/jarro2783/cxxopts)" << std::endl;
+    o << std::endl;
+    o << "    MIT License" << std::endl;
+    o << std::endl;
+    o << "    Copyright (c) 2014 Jarryd Beck" << std::endl;
+    o << std::endl;
+    o << "    Permission is hereby granted, free of charge, to any person obtaining a copy" << std::endl;
+    o << "    of this software and associated documentation files (the \"Software\"), to deal" << std::endl;
+    o << "    in the Software without restriction, including without limitation the rights" << std::endl;
+    o << "    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" << std::endl;
+    o << "    copies of the Software, and to permit persons to whom the Software is" << std::endl;
+    o << "    furnished to do so, subject to the following conditions:" << std::endl;
+    o << std::endl;
+    o << "    The above copyright notice and this permission notice shall be included in" << std::endl;
+    o << "    all copies or substantial portions of the Software." << std::endl;
+    o << std::endl;
+    o << "    THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" << std::endl;
+    o << "    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," << std::endl;
+    o << "    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" << std::endl;
+    o << "    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" << std::endl;
+    o << "    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," << std::endl;
+    o << "    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN" << std::endl;
+    o << "    THE SOFTWARE." << std::endl;
+}
